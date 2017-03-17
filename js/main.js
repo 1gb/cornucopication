c4b0a0de002c2e76b76885fe46e350e32443c804
Eve Worthington <worthington.ee@gmail.com>
2017-03-16 22:35:20 -0700

    first go at tabs and styling

--- js/main.js
+++ js/main.js
@@ -7,13 +7,14 @@ $(document).ready(function() {
     $.each( data, function() {
       events.push({
         name: this.name,
+        eventId: this.eventId,
         url: this.url,
         city: this.city,
         dateStart: moment(this.dateStart).format('MMM Do'),
         dateEnd: moment(this.dateEnd).format('MMM Do'),
         dayOfYear: moment(this.dateStart).format('DDD'),
         month: moment(this.dateStart).format('MM'),
-        monthString: moment(this.dateStart).format('MMM'),
+        monthString: moment(this.dateStart).format('MMMM'),
         datesCertain: this.datesCertain,
         type: this.type,
         desc: this.desc
@@ -38,32 +39,41 @@ $(document).ready(function() {
         events.push(events.shift());
       }
     }
-  }
+  };
 
   var format = function() {
 
-    events.forEach( function(element) {
+    events.forEach( function(element, i) {
       var thisMonth = element.monthString.toLowerCase();
+      var downIcon = '<div class="downIcon"><i class="fa fa-angle-double-down fa-3x" aria-hidden="true"></i></div>';
+      var eventUrl = '<div class="event-url"><a href="' + element.url + '" target="_blank">Visit the ' + element.name + ' Website</a></div>';
+
+      // If a month section doesn't exist, create one
 
       if ( document.getElementById(thisMonth) === null ) {
-        $('#date-list').append('<div class="month-group" id="' + thisMonth + '"><h3>' + element.monthString + '</h3><br><ul class="festival-list"><li class="item"><strong>' + element.name + '</strong><br><span class="date">' + element.dateStart + ' - ' + element.dateEnd
-          + '<span><br>' + element.city + '</li> <a href="' + element.url + '">' + element.url + '</a>' );
+        $('#date-list').append('<div class="month-group" id="' + thisMonth + '"><h3 class="month-name">' + element.monthString + '</h3><br><ul class="festival-list"><li class="item collapsed" data-toggle="collapse" data-target="#' + element.eventId + '">' + downIcon + '<div class="festival-name">' + element.name + '</div><div class="entry"><span class="date">' + element.dateStart + ' - ' + element.dateEnd + '</span> • ' + element.city +
+        '<div id="' + element.eventId + '" class="single-event collapse">' + element.desc + eventUrl + '</div></li></div>');
       } else {
-      $('#' + thisMonth + ' .festival-list').append('<li class="item"><strong>' + element.name + '</strong><br><span class="date">' + element.dateStart + ' - ' + element.dateEnd
-      + '<span><br>' + element.city + '</li> <a href="' + element.url + '">' + element.url + '</a>' )
-    };
+      $('#' + thisMonth + ' .festival-list').append('<li class="item collapsed" data-toggle="collapse" data-target="#' + element.eventId + '">' + downIcon + '<div class="festival-name">' + element.name + '</div><div class="entry"><span class="date">' + element.dateStart + ' - ' + element.dateEnd + '</span> • ' + element.city +
+      '<div id="' + element.eventId + '" class="single-event collapse">' + element.desc + eventUrl + '</div></li></div>');
+    }
   });
-}
+};
 
   $('#date-sort a').click(function (e) {
     e.preventDefault();
-    $('#map-list').hide();
-    $('#date-list').show();
+    $('#map-list').hide().toggleClass('active');
+    $('#date-list').show().toggleClass('active');
+    $('#date-sort a').addClass('active');
+    $('#map-sort a').removeClass('active');
   });
 
   $('#map-sort a').click(function (e) {
     e.preventDefault();
     $('#date-list').hide();
     $('#map-list').show();
+    $('#map-sort a').addClass('active');
+    $('#date-sort a').removeClass('active');
   });
+
 });
